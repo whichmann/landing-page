13b560dca170f16ce246a72ab95c951dfb02717c
Karol W <karol.wichmann@Outlook.com>
2020-05-02 22:01:16 +0200

    ONas component fixed for smaller devices. Kontakt updated with correct FB link

--- src/components/ONas.js
+++ src/components/ONas.js
@@ -5,17 +5,25 @@ import Col from "react-bootstrap/Col";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
+
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-import avatar1 from "./../media/1.jpg";
-import avatar2 from "./../media/2.jpg";
-import avatar3 from "./../media/3.jpg";
+
+import avatar_ap from "./../media/ap.png";
+import avatar_tp from "./../media/1.jpg";
+import avatar_ar from "./../media/ar.jpg";
+import avatar_eb from "./../media/eb.jpg";
+import avatar_kk from "./../media/kk.jpg";
+import avatar_mk from "./../media/mk.jpg";
+import avatar_pk from "./../media/pk.jpg";
 
 const useStyles = makeStyles(theme => ({
   root: {
+    minHeight: "142px",
+    paddingBottom: "15px",
+    marginBottom: "15px",
     width: "100%",
     maxWidth: "5000px",
     backgroundColor: theme.palette.background.paper
@@ -27,7 +35,8 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1
   },
   paper: {
-    padding: theme.spacing(2),
+    // padding: theme.spacing(3),
+    // paddingBottom: theme.spacing(5),
     textAlign: "center",
     color: theme.palette.text.secondary
   },
@@ -43,21 +52,26 @@ var topper = {
   marginTop: "6vh"
 };
 
+const avatarMargins = {
+  marginRight: "17px"
+}
+
 const ONas = () => {
   const classes = useStyles();
 
   return (
     <Container className={"wide-container"} style={topper}>
       <Row>
-        <Col sm={4}>
+        <Col md={6} sm={12}>
           {" "}
           <List className={classes.root + " paper-override"}>
             <ListItem alignItems="flex-start">
               <ListItemAvatar>
                 <Avatar
                   alt="Adam Poniatowski"
-                  src={avatar1}
+                  src={avatar_ap}
                   className={classes.large}
+                  style={avatarMargins}
                 />
               </ListItemAvatar>
               <ListItemText
@@ -80,107 +94,17 @@ const ONas = () => {
               />
             </ListItem>
           </List>
-          <br />
-          <List className={classes.root + " paper-override"}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                  alt="Ewa Bone"
-                  src={avatar2}
-                  className={classes.large}
-                />
-              </ListItemAvatar>
-              <ListItemText
-                primary="Ewa Bone"
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                      odpowiedzialna za przygotowanie mentalne
-                      <br />
-                    </Typography>
-                    {"Doświadczenie - 13 lat"}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          </List>
-          <br />
-          <List className={classes.root + " paper-override"}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                  alt="Paulina Kaletha"
-                  src={avatar3}
-                  className={classes.large}
-                />
-              </ListItemAvatar>
-              <ListItemText
-                primary="Paulina Kaletha"
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                      specjalistka d/s pro mocji i reklamy odpowiedzialna za
-                      język niemiecki
-                      <br />
-                    </Typography>
-                    {"Doświadczenie - 3 lata"}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          </List>
-          <br />
         </Col>
-        <Col sm={4}>
-          {" "}
-          <List className={classes.root + " paper-override"}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                  alt="Adam Poniatowski"
-                  src={avatar1}
-                  className={classes.large}
-                />
-              </ListItemAvatar>
-              <ListItemText
-                primary="Adam Poniatowski"
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                      założyciel, koordynator odpowiedzialny za matematykę oraz
-                      przygotowanie mentalne
-                      <br />
-                      <i>doświadczenie - 10 lat</i>
-                    </Typography>
-                    {"Doświadczenie - 10 lat"}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          </List>
-          <br />
+
+        <Col md={6} sm={12}>
           <List className={classes.root + " paper-override"}>
             <ListItem alignItems="flex-start">
               <ListItemAvatar>
                 <Avatar
                   alt="Ewa Bone"
-                  src={avatar2}
-                  className={classes.large}
+                  src={avatar_eb}
+                                    className={classes.large}
+                  style={avatarMargins}
                 />
               </ListItemAvatar>
               <ListItemText
@@ -193,7 +117,7 @@ const ONas = () => {
                       className={classes.inline}
                       color="textPrimary"
                     >
-                      odpowiedzialna za przygotowanie mentalne
+                      psycholog, odpowiedzialna za przygotowanie mentalne
                       <br />
                     </Typography>
                     {"Doświadczenie - 13 lat"}
@@ -202,14 +126,18 @@ const ONas = () => {
               />
             </ListItem>
           </List>
-          <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6} sm={12}>
           <List className={classes.root + " paper-override"}>
             <ListItem alignItems="flex-start">
               <ListItemAvatar>
                 <Avatar
                   alt="Paulina Kaletha"
-                  src={avatar3}
-                  className={classes.large}
+                  src={avatar_pk}
+                                    className={classes.large}
+                  style={avatarMargins}
                 />
               </ListItemAvatar>
               <ListItemText
@@ -222,7 +150,7 @@ const ONas = () => {
                       className={classes.inline}
                       color="textPrimary"
                     >
-                      specjalistka d/s pro mocji i reklamy odpowiedzialna za
+                      specjalistka d/s promocji i reklamy odpowiedzialna za
                       język niemiecki
                       <br />
                     </Typography>
@@ -232,21 +160,121 @@ const ONas = () => {
               />
             </ListItem>
           </List>
-          <br />
         </Col>
-        <Col sm={4}>
+ 
+        <Col md={6} sm={12}>
           {" "}
           <List className={classes.root + " paper-override"}>
             <ListItem alignItems="flex-start">
               <ListItemAvatar>
                 <Avatar
-                  alt="Adam Poniatowski"
-                  src={avatar1}
-                  className={classes.large}
+                  alt="Adrianna Ryńska"
+                  src={avatar_ar}
+                                    className={classes.large}
+                  style={avatarMargins}
                 />
               </ListItemAvatar>
               <ListItemText
-                primary="Adam Poniatowski"
+                primary="Adrianna Ryńska"
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="textPrimary"
+                    >
+                      odpowiedzialna za język polski
+                      <br />
+                    </Typography>
+                    {"Doświadczenie - 8 lat"}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          </List>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} sm={12}>
+          <List className={classes.root + " paper-override"}>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar
+                  alt="Martyna Klinkusz"
+                  src={avatar_mk}
+                                    className={classes.large}
+                  style={avatarMargins}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Martyna Klinkusz"
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="textPrimary"
+                    >
+                      odpowiedzialna za matematykę
+                      <br />
+                    </Typography>
+                    {"Doświadczenie - 2 lata"}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          </List>
+          </Col>
+
+          <Col md={6} sm={12}>
+          <List className={classes.root + " paper-override"}>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar
+                  alt="Kacper Kędzierski"
+                  src={avatar_kk}
+                                    className={classes.large}
+                  style={avatarMargins}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Kacper Kędzierski"
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="textPrimary"
+                    >
+                      odpowiedzialny za matematykę
+                      <br />
+                    </Typography>
+                    {"Doświadczenie - 2 lata"}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          </List>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} sm={12}>
+          {" "}
+          <List className={classes.root + " paper-override"}>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar
+                  alt="Tomek Poniatowski"
+                  src={avatar_tp}
+                                    className={classes.large}
+                  style={avatarMargins}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Tomek Poniatowski"
                 secondary={
                   <React.Fragment>
                     <Typography
@@ -265,18 +293,15 @@ const ONas = () => {
               />
             </ListItem>
           </List>
-          <br />
+        </Col>
+
+        <Col md={6} sm={12}>
+          {" "}
           <List className={classes.root + " paper-override"}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                  alt="Ewa Bone"
-                  src={avatar2}
-                  className={classes.large}
-                />
-              </ListItemAvatar>
+            <ListItem alignItems={"flex-start"}>
+
               <ListItemText
-                primary="Ewa Bone"
+                primary={<b>Dołącz do nas!</b>}
                 secondary={
                   <React.Fragment>
                     <Typography
@@ -285,40 +310,17 @@ const ONas = () => {
                       className={classes.inline}
                       color="textPrimary"
                     >
-                      odpowiedzialna za przygotowanie mentalne
                       <br />
+                      Jeśli jesteś zainteresowany dołączeniem do naszej ekipy, prześlij swoje CV na adres 
                     </Typography>
-                    {"Doświadczenie - 13 lat"}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          </List>
-          <br />
-          <List className={classes.root + " paper-override"}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                  alt="Paulina Kaletha"
-                  src={avatar3}
-                  className={classes.large}
-                />
-              </ListItemAvatar>
-              <ListItemText
-                primary="Paulina Kaletha"
-                secondary={
-                  <React.Fragment>
-                    <Typography
+                    {" rozmaturzeni@gmail.com"}
+                      <Typography
                       component="span"
                       variant="body2"
                       className={classes.inline}
                       color="textPrimary"
                     >
-                      specjalistka d/s pro mocji i reklamy odpowiedzialna za
-                      język niemiecki
-                      <br />
                     </Typography>
-                    {"Doświadczenie - 3 lata"}
                   </React.Fragment>
                 }
               />
